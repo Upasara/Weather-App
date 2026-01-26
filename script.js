a01e46518beca6f8d245896770a559b15a47335e
Upasara <mihirangaupasara2000@gmail.com>
2026-01-26 14:48:00 +0530

    created landing page

--- script.js
+++ script.js
@@ -15,43 +15,43 @@ hourlyForecastCard = document.querySelector('.hourlyForecast');
 aqiList = ['Good', 'Fair', 'Moderate', 'Poor', 'Very Poor'];
 
 function getWeatherDetails(name, lat, lon, country, state) {
-	let FORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`;
-	WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`;
-	AIR_POLLUTION_API_URL = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${api_key}`;
-	(days = [
-		'Monday',
-		'Tuesday',
-		'Wednesday',
-		'Thursday',
-		'Friday',
-		'Saturday',
-		'Sunday',
-	]),
-		(months = [
-			'Jan',
-			'Feb',
-			'Mar',
-			'Apr',
-			'May',
-			'Jun',
-			'Jul',
-			'Aug',
-			'Sep',
-			'Oct',
-			'Nov',
-			'Dec',
-		]);
+ let FORECAST_API_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${api_key}`;
+ WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}`;
+ AIR_POLLUTION_API_URL = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${api_key}`;
+ (days = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+ ]),
+  (months = [
+   'Jan',
+   'Feb',
+   'Mar',
+   'Apr',
+   'May',
+   'Jun',
+   'Jul',
+   'Aug',
+   'Sep',
+   'Oct',
+   'Nov',
+   'Dec',
+  ]);
 
-	fetch(AIR_POLLUTION_API_URL)
-		.then((res) => res.json())
-		.then((data) => {
-			let { co, no, no2, o3, so2, pm2_5, pm10, nh3 } = data.list[0].components;
-			aqiCard.innerHTML = `
+ fetch(AIR_POLLUTION_API_URL)
+  .then((res) => res.json())
+  .then((data) => {
+   let { co, no, no2, o3, so2, pm2_5, pm10, nh3 } = data.list[0].components;
+   aqiCard.innerHTML = `
               <div class="card-head">
 								<p class="airQ">Air Quality Index</p>
 								<p class="airIndex aqi-${data.list[0].main.aqi}">${
-				aqiList[data.list[0].main.aqi - 1]
-			}</p>
+    aqiList[data.list[0].main.aqi - 1]
+   }</p>
 							</div>
 							<div class="airIndices">
 								<i class='bx bx-wind bx-fade-right bx-lg' ></i>
@@ -88,16 +88,16 @@ function getWeatherDetails(name, lat, lon, country, state) {
 									<h2>${o3}</h2>
 								</div>
 							</div>`;
-		})
-		.catch(() => {
-			alert('Failed to fetch air pollution data');
-		});
+  })
+  .catch(() => {
+   alert('Failed to fetch air pollution data');
+  });
 
-	fetch(WEATHER_API_URL)
-		.then((res) => res.json())
-		.then((data) => {
-			let date = new Date();
-			currentWeatherCard.innerHTML = `
+ fetch(WEATHER_API_URL)
+  .then((res) => res.json())
+  .then((data) => {
+   let date = new Date();
+   currentWeatherCard.innerHTML = `
             <div class="current-weather">
 							<div class="details">
 								<p>Now</p>
@@ -114,25 +114,25 @@ function getWeatherDetails(name, lat, lon, country, state) {
 						<hr />
 						<div class="card-footer">
 							<p><i class="fa-solid fa-calendar-days"></i>  ${
-								days[date.getDay()]
-							}, ${date.getDate()}, ${
-				months[date.getMonth()]
-			}, ${date.getFullYear()}</p>
+        days[date.getDay()]
+       }, ${date.getDate()}, ${
+    months[date.getMonth()]
+   }, ${date.getFullYear()}</p>
 							<p><i class="fa-solid fa-location-dot"></i>  ${name}, ${country}</p>
 						</div>`;
-			let { sunrise, sunset } = data.sys,
-				{ timezone, visibility } = data,
-				{ humidity, pressure, feels_like } = data.main,
-				{ speed } = data.wind,
-				sRiseTIme = moment
-					.utc(sunrise, 'X')
-					.add(timezone, 'seconds')
-					.format('hh:mm A'),
-				sSetTime = moment
-					.utc(sunset, 'X')
-					.add(timezone, 'seconds')
-					.format('hh:mm A');
-			sunriseCard.innerHTML = `
+   let { sunrise, sunset } = data.sys,
+    { timezone, visibility } = data,
+    { humidity, pressure, feels_like } = data.main,
+    { speed } = data.wind,
+    sRiseTIme = moment
+     .utc(sunrise, 'X')
+     .add(timezone, 'seconds')
+     .format('hh:mm A'),
+    sSetTime = moment
+     .utc(sunset, 'X')
+     .add(timezone, 'seconds')
+     .format('hh:mm A');
+   sunriseCard.innerHTML = `
 							<div class="cardHead">
 								<p class="sunRS">Sunrise & Sunset</p>
 							</div>
@@ -157,104 +157,104 @@ function getWeatherDetails(name, lat, lon, country, state) {
 								</div>
 							</div>
 			`;
-			humidityVal.innerHTML = `${humidity}%`;
-			pressureVal.innerHTML = `${pressure}hPa`;
-			visibilityVal.innerHTML = `${visibility / 1000}km`;
-			speedVal.innerHTML = `${speed}m/s`;
-			feelVal.innerHTML = `${(feels_like - 273.15).toFixed(2)}&deg;C`;
-		})
-		.catch(() => {
-			alert('Failed to fetch current weather');
-		});
+   humidityVal.innerHTML = `${humidity}%`;
+   pressureVal.innerHTML = `${pressure}hPa`;
+   visibilityVal.innerHTML = `${visibility / 1000}km`;
+   speedVal.innerHTML = `${speed}m/s`;
+   feelVal.innerHTML = `${(feels_like - 273.15).toFixed(2)}&deg;C`;
+  })
+  .catch(() => {
+   alert('Failed to fetch current weather');
+  });
 
-	fetch(FORECAST_API_URL)
-		.then((res) => res.json())
-		.then((data) => {
-			let hourlyForecast = data.list;
-			hourlyForecastCard.innerHTML = '';
-			for (i = 0; i <= 7; i++) {
-				let hrForecastDate = new Date(hourlyForecast[i].dt_txt);
-				let hr = hrForecastDate.getHours();
-				let a = 'PM';
-				if (hr < 12) a = 'AM';
-				if (hr == 0) hr = 12;
-				if (hr > 12) hr = hr - 12;
-				hourlyForecastCard.innerHTML += `
+ fetch(FORECAST_API_URL)
+  .then((res) => res.json())
+  .then((data) => {
+   let hourlyForecast = data.list;
+   hourlyForecastCard.innerHTML = '';
+   for (i = 0; i <= 7; i++) {
+    let hrForecastDate = new Date(hourlyForecast[i].dt_txt);
+    let hr = hrForecastDate.getHours();
+    let a = 'PM';
+    if (hr < 12) a = 'AM';
+    if (hr == 0) hr = 12;
+    if (hr > 12) hr = hr - 12;
+    hourlyForecastCard.innerHTML += `
 						<div class="card">
 							<p>${hr} ${a}</p>
 							<img src="https://openweathermap.org/img/wn/${
-								hourlyForecast[i].weather[0].icon
-							}.png" alt="" />
+        hourlyForecast[i].weather[0].icon
+       }.png" alt="" />
 							<p>${(hourlyForecast[i].main.temp - 273.15).toFixed(2)}&deg;C</p>
 						</div>
 				`;
-			}
-			let uniqueForecastDays = [];
-			let fiveDaysForecast = data.list.filter((forecast) => {
-				let forecastDate = new Date(forecast.dt_txt).getDate();
-				if (!uniqueForecastDays.includes(forecastDate)) {
-					return uniqueForecastDays.push(forecastDate);
-				}
-			});
-			fiveDaysForecastCard.innerHTML = ``;
-			for (i = 1; i < fiveDaysForecast.length; i++) {
-				let date = new Date(fiveDaysForecast[i].dt_txt);
-				fiveDaysForecastCard.innerHTML += `
+   }
+   let uniqueForecastDays = [];
+   let fiveDaysForecast = data.list.filter((forecast) => {
+    let forecastDate = new Date(forecast.dt_txt).getDate();
+    if (!uniqueForecastDays.includes(forecastDate)) {
+     return uniqueForecastDays.push(forecastDate);
+    }
+   });
+   fiveDaysForecastCard.innerHTML = ``;
+   for (i = 1; i < fiveDaysForecast.length; i++) {
+    let date = new Date(fiveDaysForecast[i].dt_txt);
+    fiveDaysForecastCard.innerHTML += `
               <div class="forecast-item">
 								<div class="icon-wrapper">
 									<img src="https://openweathermap.org/img/wn/${
-										fiveDaysForecast[i].weather[0].icon
-									}.png" alt="" />
+          fiveDaysForecast[i].weather[0].icon
+         }.png" alt="" />
 									<span>${(fiveDaysForecast[i].main.temp - 273.15).toFixed(2)}&deg;C</span>
 								</div>
 								<p>${date.getDate()} ${months[date.getMonth()]}</p>
 								<p>${days[date.getDay()]}</p>
 							</div>`;
-			}
-		})
-		.catch(() => {
-			alert('Failed to fetch Forecast');
-		});
+   }
+  })
+  .catch(() => {
+   alert('Failed to fetch Forecast');
+  });
 }
 
 function getCity() {
-	let cityName = cityInput.value.trim();
-	cityInput.value = '';
-	if (!cityName) return;
-	let GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${api_key}`;
-	fetch(GEOCODING_API_URL)
-		.then((res) => res.json())
-		.then((data) => {
-			let { name, lat, lon, country, state } = data[0];
-			getWeatherDetails(name, lat, lon, country, state);
-		})
-		.catch(() => {
-			alert(`Failed to fetch coordinates of ${cityName}`);
-		});
+ let cityName = cityInput.value.trim();
+ cityInput.value = '';
+ if (!cityName) return;
+ let GEOCODING_API_URL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${api_key}`;
+ fetch(GEOCODING_API_URL)
+  .then((res) => res.json())
+  .then((data) => {
+   let { name, lat, lon, country, state } = data[0];
+   getWeatherDetails(name, lat, lon, country, state);
+  })
+  .catch(() => {
+   alert(`Failed to fetch coordinates of ${cityName}`);
+  });
 }
 
 function getUser() {
-	navigator.geolocation.getCurrentPosition(
-		(position) => {
-			let { latitude, longitude } = position.coords;
-			let REVERSE_GEOCODING_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${api_key}`;
+ navigator.geolocation.getCurrentPosition(
+  (position) => {
+   let { latitude, longitude } = position.coords;
+   let REVERSE_GEOCODING_URL = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${api_key}`;
 
-			fetch(REVERSE_GEOCODING_URL)
-				.then((res) => res.json())
-				.then((data) => {
-					let { name, country, state } = data[0];
-					getWeatherDetails(name, latitude, longitude, country, state);
-				})
-				.catch(() => {
-					alert('Failed to fetch Current Location');
-				});
-		},
-		(error) => {
-			if (error.code === error.PERMISSION_DENIED) {
-				alert('Location Permission needed to be ALLOW...!');
-			}
-		}
-	);
+   fetch(REVERSE_GEOCODING_URL)
+    .then((res) => res.json())
+    .then((data) => {
+     let { name, country, state } = data[0];
+     getWeatherDetails(name, latitude, longitude, country, state);
+    })
+    .catch(() => {
+     alert('Failed to fetch Current Location');
+    });
+  },
+  (error) => {
+   if (error.code === error.PERMISSION_DENIED) {
+    alert('Location Permission needed to be ALLOW...!');
+   }
+  }
+ );
 }
 
 searchBtn.addEventListener('click', getCity);
